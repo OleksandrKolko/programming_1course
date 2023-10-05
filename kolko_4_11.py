7171ed6ee05c8f5fd116de5c4bce05ea01f8013a
OleksandrKolko <144420883+OleksandrKolko@users.noreply.github.com>
2023-10-05 22:18:03 +0300

    Add lab 4

---
+++ kolko_4_11.py
@@ -0,0 +1,8 @@
+# https://www.eolymp.com/uk/submissions/14530949
+res = 0
+while True:
+    n = int(input())
+    res += n
+    if n == 0:
+        break
+print(res)
---
+++ kolko_4_8.py
@@ -0,0 +1,6 @@
+# https://www.eolymp.com/uk/submissions/14530142
+n = int(input())
+i = 1
+while i ** 3 < n:
+    print(i ** 3, end=' ')
+    i += 1
