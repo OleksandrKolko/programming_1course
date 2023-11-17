5cdc9e53fb53d6120025de37cbb2eff474965c66
OleksandrKolko <144420883+OleksandrKolko@users.noreply.github.com>
2023-11-17 23:16:51 +0200

    Update kolko_8_13.py
    Update

--- kolko_8_13.py
+++ kolko_8_13.py
@@ -1,4 +1,7 @@
-# Тут без силки на е-олімп бо там по 0. Ну головне що чісла Фібоначі знаходить і НСД.
+# https://www.eolymp.com/uk/submissions/15168987. 61%, далі не проходить по часу, навіть бінарне піднесення використав.
+from math import gcd
+
+
 def mat_scalar(A, B):
     new_mat = []
     for i in range(len(A)):
@@ -12,11 +15,14 @@ def mat_scalar(A, B):
     return new_mat
 
 
-def nsd(a, b):
-    if b == 0:
-        return a % 10 ** 8
-    else:
-        return nsd(b, a % b)
+def elevation(num):
+    result = ''
+    for i in str(bin(num))[3:]:
+        if i == '0':
+            result += 'S'
+        elif i == '1':
+            result += 'SX'
+    return result
 
 
 A = [[1, 1],
@@ -25,24 +31,37 @@ B = [[1],
      [1]]
 
 
-def fibonachi(n):
+def fib(n):
     A1 = A
     if n == 0:
-        return [0]
+        return 0
     if n == 1:
-        return [1]
+        return 1
+    if n == 2:
+        return 1
     if n % 2 == 0:
-        for i in range(int(n / 2) - 1):
-            result = mat_scalar(A1, A)
-            A1 = result
-        return mat_scalar(A1, B)[0]
+        for el in elevation(int((n - 1) / 2)):
+            if el == 'S':
+                res = mat_scalar(A1, A1)
+                A1 = res
+            elif el == 'X':
+                res = mat_scalar(A1, A)
+                A1 = res
+        return mat_scalar(A1, B)[1][0] % (10 ** 8)
     elif n % 2 != 0:
-        for i in range(int((n - 1) / 2) - 1):
-            result = mat_scalar(A1, A)
-            A1 = result
-        return mat_scalar(A1, B)[1]
+        for el in elevation(int((n - 1) / 2)):
+            if el == 'S':
+                res = mat_scalar(A1, A1)
+                A1 = res
+            elif el == 'X':
+                res = mat_scalar(A1, A)
+                A1 = res
+        return mat_scalar(A1, B)[0][0] % (10 ** 8)
 
 
 while True:
-    n, m = map(int, input().split())
-    print(nsd(*fibonachi(n), *fibonachi(m)))
+    try:
+        n, m = map(int, input().split())
+        print(fib(gcd(n, m)))
+    except EOFError:
+        break
